e8f778ae4297bc5ddc6471d9f7d22febeb98a766
deVICe7772 <f20201736@goa.bits-pilani.ac.in>
2021-03-18 21:59:16 +0530

    og

---
+++ hmm.js
@@ -0,0 +1,72 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+
+const controller = new ScrollMagic.Controller();
+
+
+let scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
+
+  video.currentTime = delay;
+}, 33.3);
+
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+   document.getElementById("navbar").style.top = "-50px";
+  } }
+
+
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {      
+      var forms = document.getElementsByClassName('needs-validation');      
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
